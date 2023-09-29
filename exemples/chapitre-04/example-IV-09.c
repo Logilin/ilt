1d1316bae56c3d00280f5e0c151bc18b067c48ba
Christophe Blaess <christophe.blaess@logilin.fr>
2023-09-29 11:32:40 +0200

    [DEV] Improve `exemple-IV-09` with a process-shared mutex.

--- exemples/chapitre-04/example-IV-09.c
+++ exemples/chapitre-04/example-IV-09.c
@@ -13,6 +13,9 @@
 #include <unistd.h>
 #include <sys/mman.h>
 
+
+#define LOOPS  1000000000
+
 struct mtx_counter {
 	int              counter;
 	pthread_mutex_t  mutex;
@@ -24,13 +27,19 @@ int main(int argc, char * argv[])
 	int fd;
 	struct mtx_counter *cnt;
 	pthread_mutexattr_t attr;
+	char buffer[16];
 
 	if (argc != 2) {
 		fprintf(stderr, "usage: %s shm_name\n", argv[0]);
 		exit(1);
 	}
 
-	if ((fd = shm_open(argv[1], O_RDWR, 0666)) < 0) {
+	if ((fd = shm_open(argv[1], O_RDWR, 0666)) >=  0) {
+
+		cnt = mmap(NULL, sizeof(struct mtx_counter), PROT_READ|PROT_WRITE,
+		                MAP_SHARED, fd, 0);
+	} else {
+
 		if ((fd = shm_open(argv[1], O_CREAT | O_RDWR, 0666)) < 0) {
 			perror(argv[1]);
 			exit(EXIT_FAILURE);
@@ -42,16 +51,21 @@ int main(int argc, char * argv[])
 		pthread_mutexattr_init(&attr);
 		pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED);
 		pthread_mutex_init(&(cnt->mutex), &attr);
-	} else {
-		cnt = mmap(NULL, sizeof(struct mtx_counter), PROT_READ|PROT_WRITE,
-		                MAP_SHARED, fd, 0);
 	}
 
-	for (;;) {
-		pthread_mutex_lock(&(cnt->mutex));
-		fprintf(stderr, "counter = %d\n", ++(cnt->counter));
-		pthread_mutex_unlock(&(cnt->mutex));
-		sleep(1);
-	}
+	fprintf(stderr, "Press `Enter` to start counting... ");
+	fgets(buffer, 15, stdin);
+
+        for (unsigned i = 0; i < LOOPS; i++){
+                pthread_mutex_lock(&(cnt->mutex));
+                cnt->counter ++;
+                pthread_mutex_unlock(&(cnt->mutex));
+        }
+
+	fprintf(stderr, "Press `Enter` to display result... ");
+	fgets(buffer, 15, stdin);
+
+	fprintf(stdout, "counter = %d\n", cnt->counter);
+
 	return 0;
 }
