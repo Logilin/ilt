c7f691ad33f6fdb5f3d5b1e445b5eadbaa96b310
Christophe Blaess <christophe.blaess@logilin.fr>
2026-04-14 08:09:28 +0200

    [RELEASE] 7.3

--- exemples/chapitre-04/example-IV-08.c
+++ exemples/chapitre-04/example-IV-08.c
@@ -1,7 +1,7 @@
 /****************************************************************************\
 ** Exemple de la formation "Temps-reel sous Linux"                          **
 **                                                                          **
-** Christophe Blaess 2012-2025                                              **
+** Christophe Blaess 2012-2026                                              **
 ** http://christophe.blaess.fr                                              **
 ** Licence GPLv2                                                            **
 \****************************************************************************/
